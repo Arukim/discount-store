bccdb0ea7057d752a31329b0d3f2dceb2502a8ac
Arukim <smelovn@gmail.com>
2015-12-22 21:53:07 +0300

    added all tests

--- store/checkout_test.go
+++ store/checkout_test.go
@@ -2,18 +2,35 @@ package store
 
 import "testing"
 
-func TestFirstCheckout(t *testing.T) {
-	totalExpected := Euro(32.50)
-	checkout := NewCheckout()
+type checkoutPair struct {
+	cart  []string
+	total Euro
+}
 
-	checkout.Scan("VOUCHER")
-	checkout.Scan("TSHIRT")
-	checkout.Scan("MUG")
+var tests = []checkoutPair{
+	{[]string{"VOUCHER", "TSHIRT", "MUG"}, Euro(32.50)},
+	{[]string{"VOUCHER", "TSHIRT", "VOUCHER"}, Euro(25.00)},
+	{[]string{"TSHIRT", "TSHIRT", "TSHIRT", "VOUCHER", "TSHIRT"}, Euro(81.00)},
+	{[]string{"VOUCHER", "TSHIRT", "VOUCHER", "VOUCHER", "MUG", "TSHIRT", "TSHIRT"}, Euro(74.50)},
+}
 
-	total := checkout.GetTotal()
-	if total != totalExpected {
-		t.Errorf("Expected %v, got %v", totalExpected, total)
+func TestAllBaseCheckouts(t *testing.T) {
+	for i, testCase := range tests {
+		checkout := NewCheckout()
+
+		for _, it := range testCase.cart {
+			checkout.Scan(it)
+		}
+
+		total := checkout.GetTotal()
+		if total != testCase.total {
+			t.Errorf("In testCase %v Expected %v, got %v",
+				i, testCase.total, total)
+		}
 	}
+}
+
+func TestSecondCheckout(t *testing.T) {
 }
 
 func TestEmptyCart(t *testing.T) {
